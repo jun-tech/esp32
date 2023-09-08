22392c9603fcd9db16fec717aa2a366b3f5f4bfe
superbeetle <271922405@qq.com>
2023-09-08 23:14:24 +0800

    u

--- vsespidf/blink/main/blink_example_main.c
+++ vsespidf/blink/main/blink_example_main.c
@@ -7,11 +7,10 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
-#include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "esp_log.h"
+#include "esp_system.h"
 #include "led_strip.h"
 #include "sdkconfig.h"
 
