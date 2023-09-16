205213f18a6c297669b3386dd610a72940c6fdda
superbeetle <271922405@qq.com>
2023-09-16 23:07:51 +0800

    task stack sample

--- freertos/example1_task/main/main.c
+++ freertos/example1_task/main/main.c
@@ -10,9 +10,11 @@
  */
 void task1(void *pvParam)
 {
+    int i = 0;
     while (1)
     {
-        ESP_LOGI("task1", "Call method : %s", "task1");
+        ESP_LOGI("task1", "Call method : %d", i);
+        i++;
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
     vTaskDelete(NULL);
@@ -56,8 +58,9 @@ void task4(void *pvParam)
 
 void app_main(void)
 {
+    TaskHandle_t xTask;
     // 简单任务，优先级设置为1（数值越大优先级越高），最优化栈配置可以查看下面的任务信息
-    xTaskCreate(task1, "task1", 1024 * 5, NULL, 1, NULL);
+    xTaskCreate(task1, "task1", 1024 * 5, NULL, 1, &xTask);
     xTaskCreate(task2, "task2", 3460, (void *)&i, 2, NULL);
     xTaskCreate(task3, "task3", 3460, (void *)str, 3, NULL);
     xTaskCreate(task4, "task4", 1024 * 5, (void *)&mystruct, 3, NULL);
@@ -70,4 +73,11 @@ void app_main(void)
     vTaskList(pcWriteBuffer);
     printf("Name    State   Priority    Stack   Num\n");
     printf("%s\n", pcWriteBuffer);
+    // 查看task堆栈使用情况
+    while (1)
+    {
+        UBaseType_t stackLeft = uxTaskGetStackHighWaterMark(xTask);
+        ESP_LOGI("task1", "stackLeft : %d", stackLeft);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
